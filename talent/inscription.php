fbb8cb31b9b30bc722f8a02cb4bbe8c63985839c
code01012019 <82946636+code01012019@users.noreply.github.com>
2021-12-30 01:11:09 +0100

    Update inscription.php

--- talent/inscription.php
+++ talent/inscription.php
@@ -8,7 +8,7 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //$sql = "INSERT INTO atbTalents (nom, mail) VALUES ('Adam', 'adam@gmail.com')";
+  //$sql = "INSERT INTO Talents (nom, mail) VALUES ('Adam', 'adam@gmail.com')";
 
     $nom = $_POST['nom'];
     $mail = $_POST['mail'];
